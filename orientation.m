67f5b69edbd941cdd21ceb09460bccb57310de83
Bernd Artmüller <berndartmueller@gmail.com>
2013-11-25 14:36:57 +0100

    adapted code to given parameter data structure

--- orientation.m
+++ orientation.m
@@ -1,17 +1,20 @@
-function frames = orientation(keypoints, octave, scale, sigma)
+function final_keypoints = orientation(keypoints, octave, scale, sigma)
 
 BINS = 36;
 
-frames = [];                  
+final_keypoints = [];                  
 window_factor = 1.5;  
 histogram = zeros(1, BINS);
 [octave_height, octave_width, s_num] = size(octave); % M is the height of image, N is the width of image; num_level is the number of scale level of the octave
 
+% amount of keypoints
 key_num = size(keypoints, 2);
-magnitudes = zeros(size(octave));
-angles = zeros(size(octave));
 
-% compute magnitude and angles
+% create empty matrix for magnitudes and angles
+magnitudes = zeros(size(octave));
+angles     = zeros(size(octave));
+
+% iterate all intervalls of given octave and compute magnitude and angles
 for sl = 1:s_num
     image = octave(:,:,sl);
     
@@ -22,35 +25,33 @@ for sl = 1:s_num
     dy = imfilter(image, dy_filter);
     
     magnitudes(:,:,sl) = sqrt( dx.^2 + dy.^2);
-    angles(:,:,sl) = atan(dy ./ dx);
+    angles(:,:,sl)     = atan(dy ./ dx);
 end
 
-
-
 % round off the cooridnates and 
-x = oframes(1,:);
-y = oframes(2,:) ;
-s = oframes(3,:);
+keypoints_x_axis = oframes(1,:);
+keypoints_y_axis = oframes(2,:) ;
+keypoints_scale = oframes(3,:);
 
-x_round = floor(oframes(1,:) + 0.5);
-y_round = floor(oframes(2,:) + 0.5);
-scales = floor(oframes(3,:) + 0.5) - smin;
+%x_round = floor(oframes(1,:) + 0.5);
+%y_round = floor(oframes(2,:) + 0.5);
+%scales = floor(oframes(3,:) + 0.5) - smin;
 
-
+% iterate each keypoint and apply window
 for keypoint=1:key_num
-    s = scales(keypoint);
-    xp= x_round(keypoint);
-    yp= y_round(keypoint);
+    % keypoint coords
+    keypoint_x = keypoints_x_axis(keypoint);
+    keypoint_y = keypoints_y_axis(keypoint);
+    keypoint_s = keypoints_scale(keypoint);
     
-    
-    sigma_gauss_window = window_factor * sigma0 * 2^(double (s / S)) ;
+    sigma_gauss_window = window_factor * sigma * 2^(double (keypoint_s / scale)) ;
     window_radius = floor(3.0* sigma_gauss_window);
     
     % iterate over all pixels in window
-    for xs = xp - window_radius : xp + window_radius
-        for ys = yp - window_radius : yp + window_radius
-            dx = (xs - x(keypoint));
-            dy = (ys - y(keypoint));
+    for xs = keypoint_x - window_radius : keypoint_x + window_radius
+        for ys = keypoint_y - window_radius : keypoint_y + window_radius
+            dx = (xs - keypoint_x);
+            dy = (ys - keypoint_y);
             
             % only continue with points which are inside the window
             if dx^2 + dy^2 <= window_radius^2
@@ -58,9 +59,9 @@ for keypoint=1:key_num
                gaussian_falloff = exp(-(dx^2 + dy^2)/(2 * sigma_gauss_window^2));
                
                % calculate the correct bin
-               bin = round( NBINS *  angles(ys, xs, s+ 1)/(2*pi) + 0.5);
+               bin = round( NBINS *  angles(ys, xs, keypoint_s + 1)/(2*pi) + 0.5);
 
-               histogram(bin) = histo(bin) + gaussian_falloff * magnitudes(ys, xs, s+ 1);
+               histogram(bin) = histo(bin) + gaussian_falloff * magnitudes(ys, xs, keypoint_s + 1);
             end
             
         end
@@ -73,12 +74,10 @@ for keypoint=1:key_num
     
     % iterate all bins with peaks
     for i = 1 : theta_bin_peaks_count
-        %theta = 2*pi * theta_indx(i) / NBINS;
+        % get angle for current keypoint
+        theta = theta_bin_peaks_indices(i);
         
-        theta = angles(y(keypoint), x(keypoint), s + 1);
-        frames = [frames, [x(keypoint) y(keypoint) s theta]'];        
+        % add keypoint with "dominant" orientation
+        final_keypoints = [final_keypoints, [keypoint_x; keypoint_y; keypoint_s; theta]];        
     end   
 end
-
-oframes = frames;
-% for each keypoint
