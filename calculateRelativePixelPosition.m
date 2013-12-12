690c96d66623922798d7a1ded7e0fe852a5f9986
Bernd Artmüller <berndartmueller@gmail.com>
2013-12-12 19:54:37 +0100

    added function header

--- calculateRelativePixelPosition.m
+++ calculateRelativePixelPosition.m
@@ -1,6 +1,17 @@
 function [ interestPoints ] = calculateRelativePixelPosition( octaveInterestPoints, currentOctaveNumber )
-%CALCULATERELATIVEPIXELPOSITION Summary of this function goes here
-%   Detailed explanation goes here
+% CALCULATERELATIVEPIXELPOSITION Calculates the pixel position of given
+% IP's relative to image size of the first octave
+% 
+% INPUT PARAMS:
+% 
+% octaveInterestPoints - found IP's in octave
+% currentOctaveNumber  - number/index of current octave
+%
+% OUTPUT PARAMS:
+% 
+% interestPoints - matrix with IP's and relative pixel position
+%
+% AUTHOR Bernd Artmueller (1127846)
 
 interestPoints = [];
 
